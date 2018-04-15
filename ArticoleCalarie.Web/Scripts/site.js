f78fbadf8cbcf400b5dd6e82c62fd0ed86d5366d
Paul Ionescu <ionepaul76@gmail.com>
2018-04-15 16:12:36 +0300

    some seo preparation

---
+++ ArticoleCalarie.Web/Scripts/site.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    $('#back').on('click', function () {
+        window.history.back();
+    });
+
+    $("#content-pager a[href]").on("click", function () {
+        showLoader();
+    });
+
+    $("#menu-main-menu a[href]").on("click", function () {
+        showLoader();
+    });
+
+    $("form").not("#logoutForm").on("submit", function () {
+        if ($(this).valid()) {
+            showLoader();
+        }
+    });
+
+    $(window).load(function () {
+        setTimeout(function () {
+            hideLoader();
+        }, 200);
+    });
+
+    if ($(window).width() <= 639) {
+        $('#appLogo').attr("src", '/images/mobile_logo.png');
+    }
+
+    $("#log-out-btn").on("click", function () {
+        $("#logoutForm").submit();
+    });
+});
+
+function showLoader() {
+    $("#spinner").removeClass("hidden");
+}
+
+function hideLoader() {
+    $("#spinner").addClass("hidden");
+}
+
+function getRelatedProducts(id) {
+    let url = window.location.origin + '/product/getrelatedproducts'
+
+    $.ajax({
+        type: "GET",
+        cache: false,
+        url: url,
+        success: function (result) {
+            $(id).html(result);
+        }
+    });
+}
